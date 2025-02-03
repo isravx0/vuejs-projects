8af3cb00df53420bdbc41f4ad51e4f0b03b62105
Israa <99071791@mydavinci.nl>
2025-02-03 13:21:27 +0100

    New project setup

---
+++ Courses/UdemyCourse/Section4/reaction-timer/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1> Ninja Reaction Time Game</h1>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  components: {},
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
